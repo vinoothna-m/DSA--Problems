3bee6112f9ca7e7a573995e2012829b10a91a197
MADITHATI VINOOTHNA <168290806+vinoothna-m@users.noreply.github.com>
2025-09-22 18:18:51 +0530

    Update reverse_array.py

--- reverse_array.py
+++ reverse_array.py
@@ -1,16 +1,15 @@
-#reverse an array
-#using slicing
-"""arr=[1,2,3,4,5,6]
+#reverse an array using slicing
+arr=[1,2,3,4,5,6]
 rev=arr[::-1]
-print(rev)"""
+print(rev)
 
 #using loop
-"""arr=[1,2,3,4,5]
+arr=[1,2,3,4,5]
 n=len(arr)
 rev=[]
 for i in range(n-1,-1,-1):
     rev.append(arr[i])
-print(rev)"""
+print(rev)
 
 #using swapping - time and space complexities=O(n),O(1)
 arr=[1,2,3,4,5,6]
@@ -21,3 +20,4 @@ while i<mid:
     arr[i],arr[n-i-1]=arr[n-i-1],arr[i]
     i+=1
 print(arr)
+
