273743de51b80bb2e782f07a53d566fe647fff72
MADITHATI VINOOTHNA <168290806+vinoothna-m@users.noreply.github.com>
2025-09-22 18:19:20 +0530

    Add files via upload

---
+++ min-max.py
@@ -0,0 +1,26 @@
+#find minimum and maximum values in an array
+"""arr=[5,12,7,3,1,19]
+min=max=arr[0]
+for n in arr:
+    if n<min: min=n
+    if n>max: max=n
+print("minimum element:", min)
+print("maximum element:", max)"""
+
+
+#using in-built methods in python
+arr=[5,12,7,3,1,19]
+print(min(arr))
+print(max(arr))
+
+
+#in function form
+def min_e(arr):
+    return min(arr)
+print(min_e(arr))
+
+def max_e(arr):
+    return max(arr)
+print(max_e(arr))
+
+arr=[5,12,7,3,1,19]
---
+++ second_largest.py
@@ -0,0 +1,26 @@
+#find second largest element in an array
+#it performs better - time and space complexities=O(n),O(1)
+"""arr=[5,12,7,3,1,19]
+largest=second_largest=float('-inf')
+for n in arr:
+    if n>largest:
+        second_largest=largest
+        largest=n
+    if n>second_largest and n!=largest:
+        second_largest=n
+if second_largest==float('-inf'):
+    print("no second largest element")
+else:
+    print(second_largest)"""
+
+
+
+#another method
+def second_largest(arr):
+    arr=list(set(arr))
+    arr.sort()
+    if len(arr)>1:
+        return arr[-2]
+    return -1
+arr=[5,12,7,3,1,19]
+print(second_largest(arr))
