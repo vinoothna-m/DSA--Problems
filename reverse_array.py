ba8d76540a393397d4f24ff2fe0a4af835b3681d
MADITHATI VINOOTHNA <168290806+vinoothna-m@users.noreply.github.com>
2025-09-22 18:17:45 +0530

    Add files via upload

---
+++ reverse_array.py
@@ -0,0 +1,23 @@
+#reverse an array
+#using slicing
+"""arr=[1,2,3,4,5,6]
+rev=arr[::-1]
+print(rev)"""
+
+#using loop
+"""arr=[1,2,3,4,5]
+n=len(arr)
+rev=[]
+for i in range(n-1,-1,-1):
+    rev.append(arr[i])
+print(rev)"""
+
+#using swapping - time and space complexities=O(n),O(1)
+arr=[1,2,3,4,5,6]
+i=0
+n=len(arr)
+mid=n//2
+while i<mid:
+    arr[i],arr[n-i-1]=arr[n-i-1],arr[i]
+    i+=1
+print(arr)
