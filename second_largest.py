6f797d7e3817fc2260d70ecb9badab5614451f09
MADITHATI VINOOTHNA <168290806+vinoothna-m@users.noreply.github.com>
2025-11-10 17:08:42 +0530

    Update second_largest.py

--- second_largest.py
+++ second_largest.py
@@ -1,5 +1,27 @@
 #find second largest element in an array
 #it performs better - time and space complexities=O(n),O(1)
+
+def getSecondLargest(arr):
+    n=len(arr)
+    if n<2:
+        return -1
+    first=second=float('-inf')
+    for num in arr:
+        if num>first:
+            second=first
+            first=num
+        elif num>second and num!=first:
+            second=num
+    if second==float('-inf'):
+        return -1
+    else:
+        return second
+arr = [12, 35, 1, 10, 34, 1]
+print(getSecondLargest(arr))    
+
+
+
+#better approach 
 arr=[5,12,7,3,1,19]
 largest=second_largest=float('-inf')
 for n in arr:
@@ -14,14 +36,31 @@ else:
     print(second_largest)
 
 
+# Python program to find the second largest element in the array
+# using two traversals
+def getSecondLargest(arr):
+    n = len(arr)
 
-#another method
-def second_largest(arr):
-    arr=list(set(arr))
-    arr.sort()
-    if len(arr)>1:
-        return arr[-2]
-    return -1
-arr=[5,12,7,3,1,19]
-print(second_largest(arr))
+    largest = -1
+    secondLargest = -1
+
+    # Finding the largest element
+    for i in range(n):
+        if arr[i] > largest:
+            largest = arr[i]
+
+    # Finding the second largest element
+    for i in range(n):
+        
+        # Update second largest if the current element is greater
+        # than second largest and not equal to the largest
+        if arr[i] > secondLargest and arr[i] != largest:
+            secondLargest = arr[i]
+    
+    return secondLargest
+
+arr = [12, 35, 1, 10, 34, 1]
+print(getSecondLargest(arr))
+
+
 
