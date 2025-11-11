5806526d79ad8bad03ccda9b35e7a8b9883f2eaa
MADITHATI VINOOTHNA <168290806+vinoothna-m@users.noreply.github.com>
2025-11-11 17:56:56 +0530

    Update move_zero.py

--- move_zero.py
+++ move_zero.py
@@ -1,21 +1,21 @@
-#Move zeros
+#Move zeros to end of array
 #shift all non zero elements to left,must done in-place
-"""non zero index keeps track of where to put next non zero elements
+"""initialize count=0 which keeps track of where to put next non zero elements
 traverse array-if element is non zero place it at that index
 after traversal fill remaining slots with zero"""
+#optimal approach - using one traversal
 
 def move_zeros(arr):
-    non_zero_index=0
+    count=0
     for i in range(len(arr)):
         if arr[i]!=0:
-            arr[non_zero_index],arr[i]=arr[i],arr[non_zero_index]
-            non_zero_index+=1
+            arr[i],arr[count]=arr[count],arr[i]
+            count+=1
     return arr
 arr=[0,1,0,3,12]
 print(move_zeros(arr))
 
-
-
 """Complexity Analysis:
 Time Complexity: O(n)
 Space Complexity: O(1)"""
+
